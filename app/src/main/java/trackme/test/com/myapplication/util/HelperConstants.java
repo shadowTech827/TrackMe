fa4e386c9e09b5dda9ad2405c8bfb6b4d4ae2d17
shadowTech827 <sarthak.sharma22@gmail.com>
2017-04-26 16:58:30 +0530

    Update HelperConstants.java

--- app/src/main/java/trackme/test/com/myapplication/util/HelperConstants.java
+++ app/src/main/java/trackme/test/com/myapplication/util/HelperConstants.java
@@ -9,7 +9,7 @@ public class HelperConstants {
     public static final String USER_PREFERENCES = "USER_PREFERENCES";
     public static final String F_KEY = "F-key" ;
     public static final String F_SECRET_KEY = "secret key for flickr";
-    public static final String FLICKR_PHOTO_API = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=d253a4fe7b37e5c68f6733fff76b15b1&format=json&nojsoncallback=1";
+    public static final String FLICKR_PHOTO_API = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=F_KEY&format=json&nojsoncallback=1";
     public static final String PHOTO_URl ="https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg";
 
 }
